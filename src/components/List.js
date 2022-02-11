cfc4aab1df2ac5d3dcb1244125786f50b4f9fd70
Angel Cornejo <self.angelcornejo@gmail.com>
2022-02-11 00:42:22 -0700

    Adds a button in lists that adds cards

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uniqid from 'uniqid';
 import Card from './Card';
 import '../styles/List.scss';
 
 const List = () => {
-  const cardOne = <Card title="c" />;
-  return (
-    <div className="list">
-      this is a list
-      {cardOne}
-      {cardOne}
-    </div>
+  const [cards, setCards] = useState([]);
+
+  const createCard = () => {
+    const newCard = <Card />;
+    setCards(cards.concat(newCard));
+  };
+
+  let list = '';
+  list = (
+    <ul className="list">
+      {cards.map(() => (
+        <li key={uniqid()}>
+          <Card />
+        </li>
+      ))}
+      <li>
+        <button type="button" className="card-creator" onClick={createCard}>
+          +Card
+        </button>
+      </li>
+    </ul>
   );
+  return list;
 };
 
 export default List;
