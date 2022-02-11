74fbcd178c4d829e7ee5b3fd0bef9c26522679bf
Angel Cornejo <self.angelcornejo@gmail.com>
2022-02-11 00:00:53 -0700

    Adds a button that creates new lists

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
+import uniqid from 'uniqid';
 import List from './components/List';
 import './styles/normalize.css';
 import './styles/App.scss';
 
 const App = () => {
-  const listOne = <List len={0} />;
-  const listTwo = <List len={0} />;
+  const [lists, setLists] = useState([]);
+
+  const createList = () => {
+    const newList = <List />;
+    setLists(lists.concat(newList));
+  };
+
   let board = '';
   board = (
     <main>
-      {listOne}
-      {listTwo}
+      <ul className="list-container">
+        {lists.map(() => (
+          <li key={uniqid()}>
+            <List />
+          </li>
+        ))}
+        <li>
+          <button type="button" className="list-creator" onClick={createList}>
+            +List
+          </button>
+        </li>
+      </ul>
     </main>
   );
   return board;
--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Card from './Card';
 import '../styles/List.scss';
 
-const List = (props) => {
-  const { len } = props;
+const List = () => {
   const cardOne = <Card title="c" />;
   return (
     <div className="list">
@@ -13,10 +11,6 @@ const List = (props) => {
       {cardOne}
     </div>
   );
-};
-
-List.propTypes = {
-  len: PropTypes.number,
 };
 
 export default List;
